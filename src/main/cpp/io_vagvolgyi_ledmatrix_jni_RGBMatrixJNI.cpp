66cc38d3e3efe84d9dc736aff3e900b1571e6dbc
Mark Vagvolgyi <mark.vagvolgyi@gmail.com>
2024-08-18 04:40:37 +0200

    Added rgbmatrix library, implemented basic wrappers for pixel drawing

--- src/main/cpp/io_vagvolgyi_ledmatrix_jni_RGBMatrixJNI.cpp
+++ src/main/cpp/io_vagvolgyi_ledmatrix_jni_RGBMatrixJNI.cpp
@@ -1,14 +1,33 @@
 #include "io_vagvolgyi_ledmatrix_jni_RGBMatrixJNI.h"
-#include <iostream>
+#include "led-matrix.h"
+
+using rgb_matrix::RGBMatrix;
+
+RGBMatrix *matrix = nullptr;
 
 void JNICALL Java_io_vagvolgyi_ledmatrix_jni_RGBMatrixJNI_initMatrix(JNIEnv *, jclass) {
-    std::cout << "Matrix initialized" << std::endl;
+    RGBMatrix::Options my_defaults;
+    my_defaults.hardware_mapping = "adafruit-hat-pwm";
+    my_defaults.chain_length = 1;
+    my_defaults.cols = 64;
+    my_defaults.rows = 64;
+    my_defaults.led_rgb_sequence = "BGR";
+
+    rgb_matrix::RuntimeOptions runtime_defaults;
+    runtime_defaults.drop_privileges = 1;
+
+    matrix = RGBMatrix::CreateFromOptions(my_defaults, runtime_defaults);
 }
 
 void JNICALL Java_io_vagvolgyi_ledmatrix_jni_RGBMatrixJNI_destroyMatrix(JNIEnv *, jclass) {
-    std::cout << "Matrix destroyed" << std::endl;
+    if (matrix != nullptr) {
+        delete matrix;
+        matrix = nullptr;
+    }
 }
 
 void JNICALL Java_io_vagvolgyi_ledmatrix_jni_RGBMatrixJNI_setPixel(JNIEnv *, jclass, jint row, jint col, jint r, jint g, jint b) {
-    std::cout << "Set pixel at (" << row << ", " << col << ") to color (" << r << ", " << g << ", " << b << ")" << std::endl;
+    if (matrix != nullptr) {
+        matrix->SetPixel(row, col, r, g, b);
+    }
 }
--- src/main/java/io/vagvolgyi/ledmatrix/Main.java
+++ src/main/java/io/vagvolgyi/ledmatrix/Main.java
@@ -3,10 +3,25 @@ package io.vagvolgyi.ledmatrix;
 import io.vagvolgyi.ledmatrix.jni.RGBMatrixJNI;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws InterruptedException {
         RGBMatrixJNI.initMatrix();
-        RGBMatrixJNI.setPixel(11, 22, 133, 144, 155);
+        byte[][] matrix = new byte[5][19];
+        matrix[0] = new byte[] {1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1};
+        matrix[1] = new byte[] {1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1};
+        matrix[2] = new byte[] {1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1};
+        matrix[3] = new byte[] {1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0};
+        matrix[4] = new byte[] {1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1};
+
+        for (int y = 0; y < matrix.length; y++) {
+            for(int x = 0; x < matrix[y].length; x++) {
+                if(matrix[y][x] == 1) {
+                    RGBMatrixJNI.setPixel(x, y, 255, 255, 255);
+                    Thread.sleep(100);
+                }
+            }
+        }
+
+        Thread.sleep(5000);
         RGBMatrixJNI.destroyMatrix();
     }
 }
